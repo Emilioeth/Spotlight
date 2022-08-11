273fc303169e504a655277e517f8a00a4a46faad
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-08-11 18:55:05 -0500

    Added to schema.sql & added user model to create our user profiles.

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// creates the users model, maybe we add encyrption using bcrypt?
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
+    });
