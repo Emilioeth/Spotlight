e25e941a42f75d18a5eefce93366e3f5991a8687
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-08-11 19:22:59 -0500

    Added bcrypt to users password phrases.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // creates the users model, maybe we add encyrption using bcrypt?
 User.init(
@@ -29,6 +30,24 @@ User.init(
           len: [4]
         }
       }
-    });
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+            }
+          },
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'user'
+    }
+    );
 
 module.exports = User;
