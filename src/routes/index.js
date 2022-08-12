850e421bde33dbbd7929fa7122b5e23c9823ba5e
twin <twin@twin.com>
2022-08-11 19:42:24 -0600

    lake of mistakes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,12 @@
-// Templates
-const template_index = require("./index.handlebars");
-// End templates
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req,res) => {
+    res.status(404).end();
+})
+
+module.exports = router;
