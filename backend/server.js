7805985825a3975f8d5a0ab091f8a29f42d70775
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-08-11 19:03:02 -0500

    Updating server.js.

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
