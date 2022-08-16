d1faa6e1c0d80b4f596e3c682a9b6d7705184dc3
twin <twin@twin.com>
2022-08-15 22:40:21 -0600

    app working, basic html rendered

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,8 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req,res) => {
-    res.status(404).end();
-})
+router.use('/', (req, res) => {
+    res.render('index');
+});
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const session = require('express-session');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001
@@ -8,22 +9,28 @@ const PORT = process.env.PORT || 3001
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+  secret: 'Password Social Music App',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 sequelize.sync({ force: false }).then(() => {
-  const sess = {
-    secret: 'Password Social Music App',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-
+  
+  app.engine('handlebars', engine());
+  app.set('view engine', 'handlebars');
+  app.set('views', './src/views');
+  
   app.use(session(sess));
-
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-
   app.use(routes);
+  app.use(express.static('public'))
+  
+
   app.listen(PORT, () => console.log('Now listening'));
 });
