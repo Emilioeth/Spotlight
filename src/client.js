6a1596d7ed128630c26ae494bf4949f519d5b139
twin <twin@twin.com>
2022-08-16 19:22:56 -0600

    fix some stuff, work on client scripts

--- src/client.js
+++ src/client.js
@@ -1,1 +1,27 @@
 import bulma from "bulma/css/bulma.css";
+import axios from "axios";
+
+function entry() {
+    
+    const loginButton = document.querySelector("#loginButton")
+    const registerButton = document.querySelector("#registerButton")
+
+    if (loginButton) {
+        loginButton.on('click',() => {
+            axios({
+                method: 'post',
+                url: '/user/12345',
+                data: {
+                  firstName: 'Fred',
+                  lastName: 'Flintstone'
+                }
+              });
+        })
+    }
+    if (registerButton) {
+        registerButton.on('click',() => {
+            
+        })
+    }
+}
+entry()
